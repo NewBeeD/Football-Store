dbf98a3be46af336c17d2e351797487a3b67e786
Danphil Daniel <danieldanphil@gmail.com>
2022-12-28 10:22:20 -0400

    bg-slide

--- frontend/src/Components/Section1.js
+++ frontend/src/Components/Section1.js
@@ -1,19 +1,61 @@
-
+import { useEffect, useState } from "react";
+import { MdSportsSoccer } from "react-icons/md";
 
 const FirstSection = () => {
+
+  const images = ['bg-img1', 'bg-img2', 'bg-img3', 'bg-img4', 'bg-img5'];
+  const counter = 0;
+
+  const [img, setImage] = useState(images[0])
+
+  const imgChange = ()=>{
+
+    if(counter < 5){setImage(images[counter])}
+    else{
+      counter = 0;
+      setImage(images[counter])
+    }
+    counter += 1;
+  }
+
+  // useEffect(()=>{
+
+  //   setInterval(imgChange, 2000)
+
+  //   return () => clearInterval(imgChange)
+  // }, [])
+
+  // setInterval()
+
+
+
+  
 
 
   return ( 
 
     <div>
 
-      <div className="mt-[3.5rem] m-auto h-[35rem] w-[100vw] md:max-w-[70rem] md:h-[40rem] shadow-2xl bg-img1 bg-cover bg-center flex justify-center items-end pb-40 text-center">
+      <div className={`${img} mt-[2.8rem] m-auto h-[35rem] w-[100vw] md:max-w-[70rem] md:h-[40rem] shadow-2xl bg-cover bg-center pb-40 text-center flex flex-col justify-center items-center pt-48`}>
 
-          <div>
-            <h1 className="text-prim5 text-5xl font-extrabold">Shop All Your Favorite Brands</h1>
+          <div className="mt-24 md:mt-48">
+            <h1 className="text-prim5 text-4xl md:text-5xl font-extrabold w-64 md:w-96">Shop All Your Favorite Brands</h1>
           </div>
 
-        
+          <div className="">
+            <button className="bg-yellow-500 p-2 rounded-full text-white mt-8 w-40 hover:scale-125 duration-300 text-center border-2">Shop Now</button>
+          </div>
+
+          <div className="md:mt-48 mt-36">
+            <ul className="flex flex-row space-x-5">
+              <li><MdSportsSoccer className="h-5 w-5 text-prim5 cursor-pointer hover:scale-125 duration-300"/></li>
+              <li><MdSportsSoccer className="h-5 w-5 text-prim5 cursor-pointer hover:scale-125 duration-300"/></li>
+              <li><MdSportsSoccer className="h-5 w-5 text-prim5 cursor-pointer hover:scale-125 duration-300"/></li>
+              <li><MdSportsSoccer className="h-5 w-5 text-prim5 cursor-pointer hover:scale-125 duration-300"/></li>
+              <li><MdSportsSoccer className="h-5 w-5 text-prim5 cursor-pointer hover:scale-125 duration-300"/></li>
+            </ul>
+          </div>
+
       </div>
 
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
       backgroundImage: {
         'img1': "url('https://images.pexels.com/photos/10923070/pexels-photo-10923070.jpeg')",
         'img2': "url('https://images.unsplash.com/photo-1597274505794-5033ff555933?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80')",
+        'img3': "url('https://images.pexels.com/photos/10923069/pexels-photo-10923069.jpeg')",
+        'img4': "url('https://images.pexels.com/photos/10923062/pexels-photo-10923062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+        'img5': "url('https://images.pexels.com/photos/7271269/pexels-photo-7271269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
       },
     },
    

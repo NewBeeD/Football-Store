2e1273a6c13ae36056364bd784c60b65ea6dadfb
Danphil Daniel <danieldanphil@gmail.com>
2023-01-03 08:27:07 -0400

    Section3-Build

--- frontend/src/Components/Section3.js
+++ frontend/src/Components/Section3.js
@@ -1,12 +1,25 @@
-import nike1 from '../images/Nike/nike1.png'
-import nike2 from '../images/Nike/nike2.png'
-import adidas1 from '../images/Adidas/adidas1.png'
+import { useEffect, useState} from 'react'
 
-// const img1 = require('../images/Nike/nike1')
 
 const Section3 = () => {
 
-  console.log();
+  const [inventory, setInventory] = useState(null)
+  
+  
+  useEffect(() => {
+
+    const fetchInventory = async () => {
+      const response = await fetch('https://localhost:5000/api/football')
+      const json = await response.json
+
+      if(response.ok){
+        setInventory(json)
+      }
+    }
+
+    fetchInventory()
+
+  }, [])
 
 
   return ( 
@@ -22,12 +35,8 @@ const Section3 = () => {
           <h2 className="text-sm w-24 md:text-base md:w-3/4">Nike Zoom Mercurial Superfly 9 Elite FG</h2>
           <h3>$280</h3>
         </div>
-
+        
       </div>
-
-
-
-
     </div>
 
 
